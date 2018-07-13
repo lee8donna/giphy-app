742b408aace5bfd3d72faed8cd5a4aa7b93a85b6
lee8donna <lee8donna@gmail.com>
2018-07-13 14:13:21 +0900

    added lodash and changed to card group view with bootstrap

--- src/components/giphy_item.js
+++ src/components/giphy_item.js
@@ -1,14 +1,12 @@
 import React, { Component } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
-const liStyle = {
-  width: "50%",
-  border: "0"
+const alertStyle = {
+  fontSize: "10px"
 };
 
-const alertStyle = {
-  fontSize: "10px",
-  padding: "5px"
+const buttonStyle = {
+  marginRight: "5px"
 };
 
 class GiphyItem extends Component {
@@ -16,56 +14,53 @@ class GiphyItem extends Component {
 
   handleClick = giphy => {
     console.log("Bookmark as favourite");
-    //this.setState({ isLinkClicked: true });
-    //console.log(this.state.isLinkedClicked);
   };
 
   render() {
     return (
-      <li className="list-group-item" style={liStyle} key={this.props.giphy.id}>
-        <div className="card">
-          <img
-            className="card-img-top"
-            src={this.props.giphy.images.fixed_height.url}
-            alt={this.props.giphy.title}
-          />
-          <div className="card-body">
-            <div className="card-text">
-              {this.state.isCopied ? (
-                <div
-                  className="alert alert-light alert-dismissable fade show"
-                  style={alertStyle}
-                >
-                  Giphy Link Copied. Go Crazy.
-                </div>
-              ) : null}
-              <CopyToClipboard
-                text={this.props.giphy.url}
-                onCopy={() => this.setState({ isCopied: true })}
-              >
-                <a
-                  className="btn btn-secondary btn-sm"
-                  onClick={() => {
-                    this.handleClick(this.props.giphy);
-                  }}
-                >
-                  Copy Gif
-                </a>
-              </CopyToClipboard>
-
-              <button
-                className="btn btn-secondary btn-sm"
-                disabled={true}
-                onClick={() => {
-                  this.handleClick(this.props.giphy);
-                }}
-              >
-                Add To Favourites
-              </button>
-            </div>
+      <div className="card">
+        <img
+          className="card-img-top"
+          src={this.props.giphy.images.fixed_height.url}
+          alt={this.props.giphy.title}
+        />
+        <div className="card-body">
+          <div className="card-text">
+            {this.state.isCopied ? (
+              <div className="alert alert-light" style={alertStyle}>
+                Giphy Link Copied. Go Crazy.
+              </div>
+            ) : null}
           </div>
         </div>
-      </li>
+        <div className="card-footer">
+          <CopyToClipboard
+            text={this.props.giphy.url}
+            onCopy={() => this.setState({ isCopied: true })}
+          >
+            <button
+              className="btn btn-secondary btn-sm"
+              style={buttonStyle}
+              onClick={() => {
+                this.handleClick(this.props.giphy);
+              }}
+            >
+              Copy Gif
+            </button>
+          </CopyToClipboard>
+
+          <button
+            className="btn btn-secondary btn-sm"
+            disabled={true}
+            style={buttonStyle}
+            onClick={() => {
+              this.handleClick(this.props.giphy);
+            }}
+          >
+            Add To Favourites
+          </button>
+        </div>
+      </div>
     );
   }
 }
--- src/components/search_result_list.js
+++ src/components/search_result_list.js
@@ -1,5 +1,6 @@
 import React from "react";
 import GiphyItem from "./giphy_item";
+import chunk from "lodash/chunk";
 
 const SearchResultList = props => {
   console.log(props.giphies.length);
@@ -7,11 +8,18 @@ const SearchResultList = props => {
     return <div>zzz....</div>;
   }
   const giphies = props.giphies;
-  const giphyItems = giphies.map(giphy => {
-    return <GiphyItem key={giphy.id} giphy={giphy} />;
+  const giphy_rows = chunk(giphies, 2);
+
+  console.log(giphy_rows);
+
+  const giphyRow = giphy_rows.map(giphy_row => {
+    const giphyItems = giphy_row.map(giphy => {
+      return <GiphyItem key={giphy.id} giphy={giphy} />;
+    });
+    return <div className="card-deck">{giphyItems}</div>;
   });
 
-  return <div className="card-group">{giphyItems}</div>;
+  return <div>{giphyRow}</div>;
 };
 
 export default SearchResultList;
